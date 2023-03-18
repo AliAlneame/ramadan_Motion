0d47d0da828d3da567f6785b59a4dff33219a6f5
ali3030 <ali3030@users.noreply.github.com>
2023-03-18 14:03:17 +0300

    ramadan Motion

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.RelativeLayout
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.airbnb.lottie.LottieAnimationView
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
